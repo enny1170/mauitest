7646effd34de83ffe078a86345f4111a7dd0a80a
enricoziehe <E.Ziehe@strube.net>
2024-07-26 10:05:57 +0200

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace mauitest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TestPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace mauitest;
+
+public partial class TestPage : ContentPage
+{
+	public TestPage()
+	{
+		InitializeComponent();
+	}
+}
